4b7eff7590fe31dae2855217b8591a0f55863993
Nathan Bell <nathanbell09@gmail.com>
2018-11-07 18:35:40 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,9 @@ def my_select(collection)
     i = 0
     new_collection = []
     while i < collection.size
-      yield collection[i] ? new_collection.push(collection[i]) : nil
+      if yield collection[i] == true
+        new_collection << collection[i]
+      end
       i += 1
     end
     new_collection
